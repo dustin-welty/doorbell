dc23a87e084263a00d62542bce2f58c04263291e
Dustin Welty <dustin.welty@prentrom.com>
2017-12-29 15:53:37 -0500

    using the config file

--- doorbell.py
+++ doorbell.py
@@ -1,5 +1,5 @@
-# import RPi.GPIO as GPIO
-# import os
+import RPi.GPIO as GPIO
+import os
 import smtplib
 from time import sleep
 import configparser
@@ -10,11 +10,11 @@ config_parse = configparser.RawConfigParser()
 configFilePath = 'config.txt'
 config_parse.read(configFilePath)
 
-channel = config_parse.get('door_bell_config', 'channel')
+channel = int(config_parse.get('door_bell_config', 'channel'))
 email_address1 = config_parse.get('door_bell_config', 'email_address1')
 email_address2 = config_parse.get('door_bell_config', 'email_address2')
 smtp_server = config_parse.get('door_bell_config', 'smtp_server')
-smtp_port = config_parse.get('door_bell_config', 'smtp_port')
+smtp_port = int(config_parse.get('door_bell_config', 'smtp_port'))
 send_address = config_parse.get('door_bell_config', 'send_address')
 send_password = config_parse.get('door_bell_config', 'send_password')
 door_bell_sound = config_parse.get('door_bell_config', 'door_bell_sound')
@@ -25,32 +25,33 @@ door_bell_sound = config_parse.get('door_bell_config', 'door_bell_sound')
 # verizon:  number@vtext.com
 # sprint:   number@page.nextel.com
 
-# # Establish a secure session with gmail's outgoing SMTP server using your gmail account
-server = smtplib.SMTP( smtp_server, smtp_port )
+# Establish a secure session with gmail's outgoing SMTP server using your gmail account
+server = smtplib.SMTP(smtp_server, smtp_port)
 
 server.starttls()
 
-server.login( send_address, send_password )
+server.login(send_address, send_password)
 
-# GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BCM)
 
-# GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-server.sendmail( send_address, email_address1, 'Door bell alerts up' )
+server.sendmail(send_address, email_address1, 'Door bell alerts up')
 
-# while True:
-# 	if GPIO.input(channel):
-# 		sleep(0.1)
-# 	else:
-# 		print('Input was LOW')
-# 		os.system('mpg123 ' + door_bell_sound)
-# 		# Send text message through SMS gateway of destination number
-# 		try:
-# 			server.sendmail( send_address, email_address1, 'Ding Dong' )
-# 			server.sendmail( send_address, email_address2, 'Ding Dong' )
-# 		except:
-# 			continue
+while True:
+    if GPIO.input(channel):
+        sleep(0.1)
+    else:
+        print('Input was LOW')
+        os.system('mpg123 ' + door_bell_sound)
+        # Send text message through SMS gateway of destination number
+        try:
+            server.sendmail(send_address, email_address1, 'Ding Dong')
+            server.sendmail(send_address, email_address2, 'Ding Dong')
+        except:
+            print('txt error')
+            continue
 
-# server.sendmail( send_address, email_address1, 'Door bell alerts down.' )
+server.sendmail(send_address, email_address1, 'Door bell alerts down.')
 
 # GPIO.cleanup()
